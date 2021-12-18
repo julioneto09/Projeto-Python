6c2d7318bf1666f31f09ddc050f5237e3ed7cc57
Júlio Gonçalo <86792617+julioneto09@users.noreply.github.com>
2021-12-18 10:48:25 -0300

    Delete drive-download-20211218T134457Z-001 directory

--- drive-download-20211218T134457Z-001/2parte.py
+++
@@ -1,5 +0,0 @@
-  #post
-  if(dicionario['tempo_segunda_inicio'] >= hora_atual and
-     dicionario['tempo_segunda_fim'] <= hora_atual and dia_semana == 'monday' )
-  
-#ccolocar sleep
--- drive-download-20211218T134457Z-001/servidor_post.py
+++
@@ -1,6 +0,0 @@
-import requests
-#-Criar um relatório-#
-arquivo = {'file': open('C:\\Users\\judon\\Desktop\\imagem1.png','rb')}
-data = {'observacao':'xacdasdasdas', 'imagens':arquivo}
-x = requests.post('http://9595-192-141-108-70.ngrok.io/api/relatorio', data = data)
-print(x.content)
